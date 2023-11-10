63f2d82dbf891b2f17d525c570463ce7e6ca15e4
Muhammad Akrabul Islam <akrabulislam1@gmail.com>
2023-11-10 22:07:57 +0600

    Different env added

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,42 @@
+import * as Joi from 'joi';
+import * as fs from 'fs';
+import * as yaml from 'js-yaml';
+
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+
+const validationSchema = Joi.object({
+  server: Joi.object({
+    port: Joi.number().min(1).max(65535).required(),
+    origin: Joi.string().required(),
+  }),
+  db: Joi.object({
+    host: Joi.string().required(),
+    type: Joi.string().valid('postgres', 'mysql', 'mariadb').required(),
+    port: Joi.number().min(1).max(65535).required(),
+    username: Joi.string().required(),
+    password: Joi.string().required(),
+    synchronize: Joi.boolean().optional(),
+  }),
+});
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      load: [
+        () => {
+          const yamlConfig = fs.readFileSync(
+            `./config/${process.env.NODE_ENV}.yml`,
+            'utf8',
+          );
+          return yaml.load(yamlConfig);
+        },
+      ],
+      validationSchema,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,15 @@
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class AppService {
+  constructor(private readonly configService: ConfigService) {}
   getHello(): string {
-    return 'Hello World!';
+    const port = this.configService.get<number>('server.port', 3000);
+    const origin = this.configService.get<string>(
+      'server.origin',
+      'http://localhost:3000',
+    );
+    return `Hello World! Server is running on port ${port}, origin is ${origin}`;
   }
 }
